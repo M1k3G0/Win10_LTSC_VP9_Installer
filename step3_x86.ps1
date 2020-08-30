fc2ee28efa8539c9d0207157887ab2b12bf686a7
M <28916828+M1k3G0@users.noreply.github.com>
2020-08-30 16:35:29 +0000

    Update step3_x86.ps1

--- step3_x86.ps1
+++ step3_x86.ps1
@@ -1,9 +1,17 @@
 cd "C:\Program Files\WindowsApps\"
-Add-AppxPackage .\Microsoft.VCLibs.140.00_14.0.27323.0_x86__8wekyb3d8bbwe.Appx
-Add-AppxPackage .\Microsoft.VP9VideoExtensions_1.0.21371.0_x86__8wekyb3d8bbwe.Appx
+Add-AppxPackage .\Microsoft.VCLibs.120.00_12.0.21005.1_x86__8wekyb3d8bbwe.appx
+Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.VCLibs.120.00_12.0.21005.1_x86__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
+Add-AppxPackage .\Microsoft.VCLibs.140.00_14.0.27810.0_x86__8wekyb3d8bbwe.Appx
+Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.VCLibs.140.00_14.0.27810.0_x86__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
+Add-AppxPackage .\Microsoft.MPEG2VideoExtension_1.0.4.0_x86__8wekyb3d8bbwe.Appx
+Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.MPEG2VideoExtension_1.0.4.0_x86__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
+Add-AppxPackage .\Microsoft.WebpImageExtension_1.0.31251.0_x86__8wekyb3d8bbwe.appx
+Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.WebpImageExtension_1.0.31251.0_x86__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
+Add-AppxPackage .\Microsoft.HEIFImageExtension_1.0.31572.0_x86__8wekyb3d8bbwe.Appx
+Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.HEIFImageExtension_1.0.31572.0_x86__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
+Add-AppxPackage .\Microsoft.AV1VideoExtension_1.1.32181.0_x86__8wekyb3d8bbwe.Appx
+Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.AV1VideoExtension_1.1.32181.0_x86__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
+Add-AppxPackage .\Microsoft.VP9VideoExtensions_1.0.31471.0_x86__8wekyb3d8bbwe.Appx
+Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.VP9VideoExtensions_1.0.31471.0_x86__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
 Add-AppxPackage .\Microsoft.WebMediaExtensions_1.0.20875.0_neutral___8wekyb3d8bbwe.AppxBundle
-Add-AppxPackage .\Microsoft.HEIFImageExtension_1.0.20982.0_x86__8wekyb3d8bbwe.Appx
-Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.VCLibs.140.00_14.0.27323.0_x86__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
-Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.VP9VideoExtensions_1.0.21371.0_x86__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
 Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.WebMediaExtensions_1.0.20875.0_x86__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
-Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.HEIFImageExtension_1.0.20982.0_x86__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
