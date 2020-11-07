df2c374b84f8c6ef4039785548720faa4776386c
M <28916828+M1k3G0@users.noreply.github.com>
2020-11-07 13:06:17 +0000

    Update step1.ps1

--- step1.ps1
+++ step1.ps1
@@ -1,9 +1,11 @@
-#cmd /c takeown.exe /F "C:\Program Files\WindowsApps\" /A
-cmd /c takeown.exe /F "C:\Program Files\WindowsApps" /A
+#cmd /c C:\Windows\System32\takeown.exe /F "C:\Program Files\WindowsApps\" /A
+cmd /c C:\Windows\System32\takeown.exe /F "C:\Program Files\WindowsApps" /A
 
-# cmd /c icacls "C:\Program Files\WindowsApps\" /grant[:r] %Username%:F
-cmd /c icacls "C:\Program Files\WindowsApps" /grant[:r] %Username%:F
+# cmd /c C:\Windows\System32\icacls.exe "C:\Program Files\WindowsApps\" /grant[:r] %Username%:F
+cmd /c C:\Windows\System32\icacls.exe "C:\Program Files\WindowsApps" /grant[:r] %Username%:F
 
-cmd /c reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /v "AllowAllTrustedApps" /t REG_DWORD /d 1 /f
+# add option to install
+cmd /c C:\Windows\System32\reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /v "AllowAllTrustedApps" /t REG_DWORD /d 1 /f
 
+# delete all old Apps
 Get-AppxPackage -AllUsers | Remove-AppxPackage
