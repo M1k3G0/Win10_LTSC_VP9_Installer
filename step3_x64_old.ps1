fbae26d8f4923fcf0f189748cc66372992bc55de
M <28916828+M1k3G0@users.noreply.github.com>
2020-11-07 13:00:31 +0000

    Update step3_x64_old.ps1

--- step3_x64_old.ps1
+++ step3_x64_old.ps1
@@ -1,4 +1,5 @@
 cd "C:\Program Files\WindowsApps\"
+
 Add-AppxPackage .\Microsoft.VCLibs.140.00_14.0.27323.0_x86__8wekyb3d8bbwe.Appx
 Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.VCLibs.140.00_14.0.27323.0_x86__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
 Add-AppxPackage .\Microsoft.VCLibs.140.00_14.0.27323.0_x64__8wekyb3d8bbwe.Appx
@@ -7,3 +8,6 @@ Add-AppxPackage .\Microsoft.VP9VideoExtensions_1.0.21371.0_x64__8wekyb3d8bbwe.Ap
 Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.VP9VideoExtensions_1.0.21371.0_x64__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
 Add-AppxPackage .\Microsoft.WebMediaExtensions_1.0.20875.0_neutral___8wekyb3d8bbwe.AppxBundle
 Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.WebMediaExtensions_1.0.20875.0_x64__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
+
+#cmd /c C:\Windows\System32\icacls.exe "C:\Program Files\WindowsApps\" /remove[:g] %Username%
+cmd /c C:\Windows\System32\icacls.exe "C:\Program Files\WindowsApps" /remove[:g] %Username%
