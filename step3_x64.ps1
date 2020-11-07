85cb8cf1b6ca8c9e005a981225e7125506de5d2a
M <28916828+M1k3G0@users.noreply.github.com>
2020-11-07 11:13:04 +0000

    Update step3_x64.ps1

--- step3_x64.ps1
+++ step3_x64.ps1
@@ -8,6 +8,10 @@ Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.VCLibs.140.00_
 Add-AppxPackage .\Microsoft.VCLibs.140.00_14.0.27810.0_x64__8wekyb3d8bbwe.Appx
 Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.VCLibs.140.00_14.0.27810.0_x64__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
 Add-AppxPackage .\Microsoft.MPEG2VideoExtension_1.0.4.0_x64__8wekyb3d8bbwe.Appx
+Add-AppxPackage .\Microsoft.VCLibs.140.00.UWPDesktop_14.0.27810.0_x86__8wekyb3d8bbwe.appx
+Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.VCLibs.140.00.UWPDesktop_14.0.27810.0_x86__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
+Add-AppxPackage .\Microsoft.VCLibs.140.00.UWPDesktop_14.0.27810.0_x64__8wekyb3d8bbwe.appx
+Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.VCLibs.140.00.UWPDesktop_14.0.27810.0_x64__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
 Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.MPEG2VideoExtension_1.0.4.0_x64__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
 Add-AppxPackage .\Microsoft.AV1VideoExtension_1.1.32181.0_x64__8wekyb3d8bbwe.Appx
 Add-AppxPackage -register "C:\Program Files\WindowsApps\Microsoft.AV1VideoExtension_1.1.32181.0_x64__8wekyb3d8bbwe\AppxManifest.xml" -DisableDevelopmentMode
