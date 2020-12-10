f20f4ee6e4a959dadc02175ad2cb2216e84462fb
M <28916828+M1k3G0@users.noreply.github.com>
2020-12-10 15:09:29 +0000

    Update step1.ps1

--- step1.ps1
+++ step1.ps1
@@ -1,13 +1,13 @@
-# folder's owner is admin
-C:\Windows\System32\cmd.exe /c C:\Windows\System32\takeown.exe /F "C:\Program Files\WindowsApps" /A
+# folder's owner is admins group
+C:\Windows\System32\cmd.exe /c C:\Windows\System32\takeown.exe /F "C:\Program Files\WindowsApps" /A /R
 # C:\Windows\System32\cmd.exe /c C:\Windows\System32\takeown.exe /F "C:\Program Files\WindowsApps\" /A
 
 # full access for current user (temporary):
-C:\Windows\System32\cmd.exe /c C:\Windows\System32\icacls.exe "C:\Program Files\WindowsApps" /grant[:r] %Username%:F
+C:\Windows\System32\cmd.exe /c C:\Windows\System32\icacls.exe "C:\Program Files\WindowsApps" /grant[:r] %USERDOMAIN%\%Username%:F
 # C:\Windows\System32\cmd.exe /c C:\Windows\System32\icacls.exe "C:\Program Files\WindowsApps\" /grant[:r] %Username%:F
 
 # add option to install
-cmd /c C:\Windows\System32\reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /v "AllowAllTrustedApps" /t REG_DWORD /d 1 /f
+C:\Windows\System32\cmd.exe /c C:\Windows\System32\reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /v "AllowAllTrustedApps" /t REG_DWORD /d 1 /f
 
 # delete all old Apps
 Get-AppxPackage -AllUsers | Remove-AppxPackage
