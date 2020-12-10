57b2fe7783a3f6ab7f6d39fc0d6eabffc597d9c5
M <28916828+M1k3G0@users.noreply.github.com>
2020-12-10 15:35:26 +0000

    Update step1.ps1

--- step1.ps1
+++ step1.ps1
@@ -2,9 +2,17 @@
 C:\Windows\System32\cmd.exe /c C:\Windows\System32\takeown.exe /F "C:\Program Files\WindowsApps" /A /R
 # C:\Windows\System32\cmd.exe /c C:\Windows\System32\takeown.exe /F "C:\Program Files\WindowsApps\" /A
 
-# full access for current user (temporary):
+
+
+# remove old access for current user (if exists)
+C:\Windows\System32\cmd.exe /c C:\Windows\System32\icacls.exe "C:\Program Files\WindowsApps" /remove[:g] %USERDOMAIN%\%Username%
+# C:\Windows\System32\cmd.exe /c C:\Windows\System32\icacls.exe "C:\Program Files\WindowsApps\" /remove[:g] %Username%
+
+# full access for current user (temporary)
 C:\Windows\System32\cmd.exe /c C:\Windows\System32\icacls.exe "C:\Program Files\WindowsApps" /grant[:r] %USERDOMAIN%\%Username%:F
 # C:\Windows\System32\cmd.exe /c C:\Windows\System32\icacls.exe "C:\Program Files\WindowsApps\" /grant[:r] %Username%:F
+
+
 
 # add option to install
 C:\Windows\System32\cmd.exe /c C:\Windows\System32\reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /v "AllowAllTrustedApps" /t REG_DWORD /d 1 /f
